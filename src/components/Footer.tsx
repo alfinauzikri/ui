2e79ea37eed5266f24cb97aac45e7dd7fa60779e
Alfin Auzikri <iyalfin@gmail.com>
2025-07-07 21:05:11 +0700

    fix: update Footer component to improve text alignment and responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <footer className="py-4 md:py-6">
             <div className="container-wrapper">
-                <p className="text-center text-xs text-pretty md:text-sm text-muted-foreground">
+                <p className="text-center text-xs text-pretty md:text-sm max-w-3xs md:max-w-2xl mx-auto text-muted-foreground">
                     Built by <a className="font-medium underline" target="_blank" href="https://alfauzikri.my.id">@alfinauzikri</a> from <a className="font-medium underline" target="_blank" href="https://www.merakit.co.id">@merakit</a>.
                     View the source code on <a className="font-medium underline" target="_blank" href="https://github.com/alfinauzikri/ui">GitHub</a>.
                 </p>
